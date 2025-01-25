395f4de86dcef75f21c6cad48c98af73da80ab02
Christine Wu <christinewwuu@gmail.com>
2025-01-25 16:08:36 -0500

    form kind of works!

--- server.py
+++ server.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template, request
 from grocery import get_value
+from waitress import serve
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-    return "Hello World"
+    return render_template('index.html')
+
+@app.route('/grocery')
+def get_grocery():
+    # city = request.args.get('city')
+    # grocery_data = get_current_grocery(city)
+    return render_template(
+        "grocery.html",
+        title="New York",
+        status="Cloudy",
+        temp="15",
+        feels_like="12"
+    )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    serve(app, host="0.0.0.0", port=8000)
