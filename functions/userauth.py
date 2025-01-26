ac2e538b520165efac6bf7450d29b129c34feb50
Emerson Lin <emersonlin.gh@gmail.com>
2025-01-26 03:33:31 -0500

    fixed login/registration authentication bugs

--- functions/userauth.py
+++ functions/userauth.py
@@ -36,12 +36,30 @@ def register():
 
         # hash password
         hashed_pw = generate_password_hash(password)
+
         # add email + hashed password to user db
-        mydb = db_connection()
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user=os.environ.get("db_user"),
+            password=os.environ.get("db_pass"),
+            database="grocerydb"
+        )
         mycursor = mydb.cursor()
-        reg_query = "INSERT INTO users (email, password) VALUES (%s, %s)"
-        mycursor.execute(reg_query, (email, hashed_pw))
-        mydb.close()
+        try:
+            reg_query = "INSERT INTO users (email, password) VALUES (%s, %s)"
+            mycursor.execute(reg_query, (email, hashed_pw))
+            mydb.commit()
+
+        except mysql.connector.errors.IntegrityError as err:
+            # Handle integrity errors like duplicate key
+            if '1062' in str(err):  # Duplicate entry error code
+                return render_template("register.html", message="Email already exists. Please try another one.")
+            else:
+                return render_template("register.html", message=f"An error occurred: {err}")
+
+        finally:
+            mycursor.close()
+            mydb.close()
 
         return redirect("/login")
 
@@ -55,14 +73,10 @@ def login():
         password = request.form.get("password")
 
         # look up email in user db
-            # host="localhost",
-            # user=os.environ.get("db_user"),
-            # password=os.environ.get("db_pass"),
-            # database="grocerydb"
         mydb = db_connection()
         mycursor = mydb.cursor()
         login_query = "SELECT email, password FROM users WHERE email = %s"
-        mycursor.execute(login_query, email)
+        mycursor.execute(login_query, (email,))
         userdata = mycursor.fetchall()
         mydb.close()
 
@@ -72,10 +86,10 @@ def login():
                 session["user_id"] = email
                 return redirect("/home")
             else:
-                return render_template(login.html, message="Invalid password.")
+                return render_template("login.html", message="Invalid password.")
 
         else:
-            return render_template(login.html, message="No account with that email exists.")
+            return render_template("login.html", message="No account with that email exists.")
 
     return render_template("login.html")
 
--- server.py
+++ server.py
@@ -54,11 +54,21 @@ def register():
             database="grocerydb"
         )
         mycursor = mydb.cursor()
-        reg_query = "INSERT INTO users (email, password) VALUES (%s, %s)"
-        mycursor.execute(reg_query, (email, hashed_pw))
-        mydb.commit()
-        mycursor.close()
-        mydb.close()
+        try:
+            reg_query = "INSERT INTO users (email, password) VALUES (%s, %s)"
+            mycursor.execute(reg_query, (email, hashed_pw))
+            mydb.commit()
+
+        except mysql.connector.errors.IntegrityError as err:
+            # Handle integrity errors like duplicate key
+            if '1062' in str(err):  # Duplicate entry error code
+                return render_template("register.html", message="Email already exists. Please try another one.")
+            else:
+                return render_template("register.html", message=f"An error occurred: {err}")
+
+        finally:
+            mycursor.close()
+            mydb.close()
 
         return redirect("/login")
 
@@ -75,10 +85,8 @@ def login():
         mydb = db_connection()
         mycursor = mydb.cursor()
         login_query = "SELECT email, password FROM users WHERE email = %s"
-        mycursor.execute(login_query, email)
-        mydb.commit()
+        mycursor.execute(login_query, (email,))
         userdata = mycursor.fetchall()
-        mycursor.close()
         mydb.close()
 
         # check if email, password valid
@@ -87,10 +95,10 @@ def login():
                 session["user_id"] = email
                 return redirect("/home")
             else:
-                return render_template(login.html, message="Invalid password.")
+                return render_template("login.html", message="Invalid password.")
 
         else:
-            return render_template(login.html, message="No account with that email exists.")
+            return render_template("login.html", message="No account with that email exists.")
 
     return render_template("login.html")
 
