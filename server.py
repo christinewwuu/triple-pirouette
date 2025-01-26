9dbb1ed878ec85a5084a72f13964ac8cf9d52746
Christine Wu <christinewwuu@gmail.com>
2025-01-26 02:25:49 -0500

    working register frontend

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, render_template, redirect, session
 from grocery import get_value
 from waitress import serve
+import os
+from werkzeug.security import generate_password_hash
+import mysql.connector
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('fb_key')
 
 @app.route('/')
 @app.route('/index')
@@ -13,17 +17,33 @@ def index():
 def home():
     return render_template("home.html")
 
-@app.route('/grocery')
-def get_grocery():
-    # city = request.args.get('city')
-    # grocery_data = get_current_grocery(city)
-    return render_template(
-        "grocery.html",
-        title="New York",
-        status="Cloudy",
-        temp="15",
-        feels_like="12"
-    )
+# user sign up/registration
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # validate registration data
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        if not (email and password):
+            return render_template("register.html", message="All fields are required.")
+
+        # hash password
+        hashed_pw = generate_password_hash(password)
+
+        # add email + hashed password to user db
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user=os.environ.get("db_user"),
+            password=os.environ.get("db_pass"),
+            database="grocerydb"
+        )
+        mycursor = mydb.cursor()
+        mycursor.execute(f"INSERT INTO users (email, password) VALUES ({email}, {hashed_pw})")
+
+        return redirect("/login")
+
+    return render_template("register.html")
 
 if __name__ == "__main__":
     # serve(app, host="0.0.0.0", port=8000)
